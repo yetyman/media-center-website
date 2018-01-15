2d4dc60d6c73f58298836b758076af7be655db9d
Jody Sowald <yetyman34@gmail.com>
2018-01-14 22:45:51 -0500

    code is up to PEP8 Standards

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -132,8 +132,7 @@ def create_movie_tiles_content(movies):
     for movie in movies:
         # Extract the youtube ID from the url
         youtube_id_match = re.search(r'(?<=v=)[^&#]+', movie.trailer_youtube_url)
-        youtube_id_match = youtube_id_match or re.search(r'(?<=be/)[^&#]+',
-                                                         movie.trailer_youtube_url)
+        youtube_id_match = youtube_id_match or re.search(r'(?<=be/)[^&#]+', movie.trailer_youtube_url)
         trailer_youtube_id = youtube_id_match.group(0) if youtube_id_match else None
 
         # Append the tile for the movie with its content filled in
--- media.py
+++ media.py
@@ -1,8 +1,10 @@
 """defines media types"""
+
+
 class Movie(object):
-    """a light container for movie as it pertains to the entertainment center module's usage"""
+    """a light container for movie as it pertains
+    to the entertainment center module's usage"""
     def __init__(self, movie_title, movie_trailer, poster_url):
         self.title = movie_title
         self.trailer_youtube_url = movie_trailer
         self.poster_image_url = poster_url
-   
